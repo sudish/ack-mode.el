b58b5a98e38a6ee3bd9146cf0d3c85c9a300adee
Sudish Joseph <sudish@gmail.com>
2012-03-04 23:25:47 -0500

    Use ack-mode- as the prefix everywhere.

--- ack-mode.el
+++ ack-mode.el
@@ -32,19 +32,19 @@
   :package-version "1"
   :group 'ack-mode)
 
-(defcustom ack-program-name "ack"
+(defcustom ack-mode-program-name "ack"
   "The name of the ack binary."
   :type '(string)
   :require 'ack-mode
   :group 'ack-mode)
 
-(defcustom ack-arguments '("--group" "--nopager" "--nocolor")
+(defcustom ack-mode-arguments '("--group" "--nopager" "--nocolor")
   "Arguments for ack, used on each search."
   :type '(repeat string)
   :require 'ack-mode
   :group 'ack-mode)
 
-(defcustom ack-root-directory-function 'ack-default-directory
+(defcustom ack-mode-root-directory-function 'ack-mode-default-directory
   "Function returning the directory to start the search in.
 
 Useful in conjunction with a project root package - use this to
@@ -53,7 +53,7 @@ have ack begin its search at the root of your project."
   :require 'ack-mode
   :group 'ack-mode)
 
-(defcustom ack-header-line-face 'mode-line-buffer-id
+(defcustom ack-mode-header-line-face 'mode-line-buffer-id
   "Face used for header lines in ack buffers."
   :type '(face)
   :require 'ack-mode
@@ -73,56 +73,58 @@ have ack begin its search at the root of your project."
   :group 'ack-mode)
 
 
-(defvar ack-font-lock-keywords `((,ack-mode-file-regexp . (0 font-lock-keyword-face t))
-				 (,ack-mode-line-regexp . font-lock-variable-name-face)))
+(defvar ack-mode-font-lock-keywords
+  `((,ack-mode-file-regexp . (0 font-lock-keyword-face t))
+    (,ack-mode-line-regexp . font-lock-variable-name-face)))
 
 ;; The buffer-locals in an ack-mode buffer
-(defvar ack-local-font-lock-keywords)
+(defvar ack-mode-local-font-lock-keywords)
 
 (define-derived-mode ack-mode special-mode "Ack"
   "Major mode for ack search results.")
 
-(define-key ack-mode-map [return]      'ack-visit-result)
-(define-key ack-mode-map [(shift tab)] 'ack-previous-file)
-(define-key ack-mode-map [tab]         'ack-next-file)
+(define-key ack-mode-map [return]      'ack-mode-visit-result)
+(define-key ack-mode-map [(shift tab)] 'ack-mode-previous-file)
+(define-key ack-mode-map [tab]         'ack-mode-next-file)
 
 ;;;###autoload
 (defun ack (search-string)
-  "Runs `ack-program-name' and displays the result in a browsable buffer."
+  "Runs `ack-mode-program-name' and displays the result in a browsable buffer."
   (interactive (list (grep-read-regexp)))
-  (let* ((pwd (or (and (functionp ack-root-directory-function)
-		       (funcall ack-root-directory-function))
+  (let* ((pwd (or (and (functionp ack-mode-root-directory-function)
+		       (funcall ack-mode-root-directory-function))
 		  default-directory))
-	 (buf (ack-start-ack-process search-string pwd)))
+	 (buf (ack-mode-start-ack-process search-string pwd)))
     (switch-to-buffer-other-window buf)))
 
-(defun ack-default-directory ()
+(defun ack-mode-default-directory ()
   "Returns default (current) directory of the current buffer.
 
-Useful as value for `ack-root-directory-function'."
+Useful as value for `ack-mode-root-directory-function'."
   default-directory)
 
-(defun ack-start-ack-process (search-string dir)
+(defun ack-mode-start-ack-process (search-string dir)
   (let ((buf (generate-new-buffer (format "*ack \"%s\"*" search-string))))
     (save-current-buffer
       (set-buffer buf)
       (ack-mode)
-      (setq header-line-format (ack-header dir))
+      (setq header-line-format (ack-mode-header dir))
       (hack-dir-local-variables-non-file-buffer)
       (setq default-directory dir)
 
-      (set (make-local-variable 'ack-local-font-lock-keywords)
+      (set (make-local-variable 'ack-mode-local-font-lock-keywords)
 	   (cons `(,search-string . font-lock-string-face)
-		 ack-font-lock-keywords))
-      (setq font-lock-defaults '(ack-local-font-lock-keywords t t))
+		 ack-mode-font-lock-keywords))
+      (setq font-lock-defaults '(ack-mode-local-font-lock-keywords t t))
       (font-lock-mode 1)
 
       (let ((proc (apply 'start-process (buffer-name buf) buf
-			 ack-program-name (append ack-arguments (list search-string)))))
-	(set-process-filter   proc 'ack-process-filter)))
+			 ack-mode-program-name
+			 (append ack-mode-arguments (list search-string)))))
+	(set-process-filter proc 'ack-mode-process-filter)))
     buf))
 
-(defun ack-process-filter (proc string)
+(defun ack-mode-process-filter (proc string)
   (let ((buf (process-buffer proc))
 	(inhibit-read-only t))
     (when (buffer-live-p buf)
@@ -132,12 +134,12 @@ Useful as value for `ack-root-directory-function'."
 	  (insert string)
 	  (set-marker (process-mark proc) (point)))))))
 
-(defun ack-visit-result ()
+(defun ack-mode-visit-result ()
   "Visit file and line displayed on current line in search results buffer.
 
 The file is visited in a separate window with the current line centered."
   (interactive)
-  (let* ((file (car (ack-find-file-group 'current)))
+  (let* ((file (car (ack-mode-find-file-group 'current)))
 	 (buf (and file (get-file-buffer file)))
 	 (line (save-excursion
 		 (save-match-data
@@ -145,12 +147,12 @@ The file is visited in a separate window with the current line centered."
 		   (when (looking-at ack-mode-line-regexp)
 		     (string-to-number (match-string 1)))))))
     (cond ((bufferp buf)
-	   (ack-display-buffer buf line))
+	   (ack-mode-display-buffer buf line))
 	  ((and file (file-readable-p file))
-	   (ack-display-buffer (find-file-noselect file) line))
+	   (ack-mode-display-buffer (find-file-noselect file) line))
 	  (t (when file (error "Couldn't show file %s" file))))))
 
-(defun ack-display-buffer (buf line)
+(defun ack-mode-display-buffer (buf line)
   "Display line number `line' of buffer `buf' in some visible window."
   (when (bufferp buf)
     (save-selected-window
@@ -160,9 +162,9 @@ The file is visited in a separate window with the current line centered."
 	(goto-char (point-min))
 	(forward-line (1- (or line 0)))
 	(recenter)
-	(ack-temporarily-highlight-line)))))
+	(ack-mode-temporarily-highlight-line)))))
 
-(defun ack-find-file-group (which)
+(defun ack-mode-find-file-group (which)
   "Returns (PATH . LOCATION) for the file group specified by `which'.
 `which' may be 'next, 'current. Any other value is treated as 'previous.
 
@@ -179,35 +181,35 @@ buffer where the file group begins."
 	     (cons (substring-no-properties (match-string 1)) (point)))
 	    (t (cons nil old-point))))))
 
-(defun ack-next-file ()
+(defun ack-mode-next-file ()
   "Go to the next file in the search results buffer."
   (interactive)
-  (let ((location (cdr (ack-find-file-group 'next))))
+  (let ((location (cdr (ack-mode-find-file-group 'next))))
     (when location
       (goto-char location))))
 
-(defun ack-previous-file ()
+(defun ack-mode-previous-file ()
   "Go to the last file in the search results buffer."
   (interactive)
-  (let ((location (cdr (ack-find-file-group 'previous))))
+  (let ((location (cdr (ack-mode-find-file-group 'previous))))
     (when location
       (goto-char location))))
 
-(defun ack-header (dir)
+(defun ack-mode-header (dir)
   "Returns a string suitable for `header-line-format'."
   (let* ((shortened (replace-regexp-in-string dir ".../" default-directory))
 	 (header (format "root: %s, pwd: %s, %%s" dir shortened)))
-    `(:propertize ,header face ,ack-header-line-face)))
+    `(:propertize ,header face ,ack-mode-header-line-face)))
 
 ;; Support for temporarily highlighting current line.  The highlight is
 ;; removed when the window configuration changes.
 (defvar ack-mode-overlay (make-overlay 0 0))
 (overlay-put ack-mode-overlay 'face isearch-face)
 
-(defun ack-temporarily-highlight-line ()
+(defun ack-mode-temporarily-highlight-line ()
   (move-overlay ack-mode-overlay (line-beginning-position) (line-end-position))
-  (add-hook 'window-configuration-change-hook 'ack-remove-highlight))
+  (add-hook 'window-configuration-change-hook 'ack-mode-remove-highlight))
 
-(defun ack-remove-highlight ()
+(defun ack-mode-remove-highlight ()
   (delete-overlay ack-mode-overlay)
-  (remove-hook 'window-configuration-change-hook 'ack-remove-highlight))
+  (remove-hook 'window-configuration-change-hook 'ack-mode-remove-highlight))
